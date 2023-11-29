8f130fe8ab6dd19d2bd9da273354c621f476bebd
hadi <hadisdegh@yahoo.com>
2023-11-29 23:26:46 +0330

    init

---
+++ bulma/src/App.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import Header from "./layouts/Header.vue";
+import Aside from "./layouts/Aside.vue";
+import Footer from "./layouts/Footer.vue";
+import Navigation from "./components/Navigation.vue";
+</script>
+
+<template>
+  <header>
+    <Header></Header>
+  </header>
+  <main>
+    <section class="pt-5">
+      <div class="container shadow section-sm rounded">
+        <div class="columns is-multiline">
+          <!-- sidebar -->
+          <div class="column is-3-desktop is-12-tablet">
+            <Aside></Aside>
+          </div>
+          <!-- body -->
+          <div class="column is-8-desktop is-12-tablet">
+            <div class="px-4">
+              <h2 class="mb-4 has-text-weight-medium">Basic Startup</h2>
+              <!-- main content -->
+           
+                <RouterView />
+             
+              <!-- navigation -->
+              <Navigation></Navigation>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer>
+    <Footer></Footer>
+  </footer>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ bulma/src/layouts/Aside.vue
@@ -0,0 +1,102 @@
+<template>
+     <ul class="sidenav">
+              <li title="Basic Startup" class="sidelist parent active">
+                <a href="list.html">Basic Startup</a>
+                <ul>
+                  <li class="sidelist">
+                    <a href="single.html">Elements</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Installation</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Configuration</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Customization</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Requerments</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="sidelist">
+                <a href="list.html">Account Bill</a>
+                <ul>
+                  <li class="sidelist">
+                    <a href="single.html">Linux</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Mac OS</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Routing</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Ubuntu</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Windows</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="sidelist">
+                <a href="list.html">Our Features</a>
+                <ul>
+                  <li class="sidelist">
+                    <a href="single.html">Linux</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Mac OS</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Routing</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Ubuntu</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Windows</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="sidelist">
+                <a href="list.html">Theme Facility</a>
+                <ul>
+                  <li class="sidelist">
+                    <a href="single.html">Linux</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Mac OS</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Routing</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Ubuntu</a>
+                  </li>
+
+                  <li class="sidelist">
+                    <a href="single.html">Windows</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+</template>
+           
+        
